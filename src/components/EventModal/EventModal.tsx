04dc4b82eac61df68e250dd6f5b6579f70d1b04a
ex1one <vyvf11@gmail.com>
2022-05-14 13:41:34 +0500

    Improvements

--- src/components/EventModal/EventModal.tsx
+++ src/components/EventModal/EventModal.tsx
@@ -1,9 +1,12 @@
 import React, { FC } from 'react';
 import {
-  Box, Modal, Typography,
+  Box, Button, Modal, Typography,
 } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
+import DeleteIcon from '@mui/icons-material/Delete';
 import { IEvent } from '../../models/IEvent';
+import styles from './eventModal.module.scss';
+import useActions from '../../hooks/useActions';
 
 interface IModalProps {
   events: IEvent[];
@@ -11,55 +14,69 @@ interface IModalProps {
   show: boolean;
 }
 
-const EventModal: FC <IModalProps> = ({ show, events, showNotifications }) => (
-  <Modal
-    open={show}
-    onClose={showNotifications}
-  >
-    <Box sx={{
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
-      width: 400,
-      backgroundColor: '#eee',
-      border: '1px solid #000',
-      boxShadow: 24,
-      p: 4,
-    }}
+const EventModal: FC <IModalProps> = ({ show, events, showNotifications }) => {
+  const { removeEvent } = useActions();
+
+  return (
+    <Modal
+      open={show}
+      onClose={showNotifications}
     >
-      <Typography variant="h6" component="h2">
-        События
-        <CloseIcon
-          onClick={showNotifications}
-          sx={{
-            float: 'right',
-            cursor: 'pointer',
-            width: '20px',
-          }}
-        />
-      </Typography>
-      <Box sx={{ mt: 2 }}>
-        {events.length ? (
-          <>
-            {events.map((ev) => (
-              <div key={ev.description}>
-                <p>
-                  Дата события:
-                  {ev.date}
-                </p>
-                <p>
-                  Описание события:
-                  {ev.description}
-                </p>
-              </div>
-            ))}
-          </>
-        )
-          : (<h3>У вас нет новых событий</h3>)}
+      <Box sx={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        backgroundColor: '#eee',
+        border: '1px solid #000',
+        boxShadow: 24,
+        p: 4,
+      }}
+      >
+        <Typography variant="h6" component="h2">
+          События
+          <CloseIcon
+            onClick={showNotifications}
+            sx={{
+              float: 'right',
+              cursor: 'pointer',
+              width: '20px',
+            }}
+          />
+        </Typography>
+        <Box sx={{ mt: 2 }}>
+          {events.length ? (
+            <>
+              {events.map((ev) => (
+                <div
+                  className={styles.event}
+                  key={ev.description}
+                >
+                  <p>
+                    Дата события:
+                    {ev.date}
+                  </p>
+                  <p>
+                    Описание события:
+                    {ev.description}
+                  </p>
+                  <Button
+                    onClick={() => removeEvent(ev)}
+                    variant="text"
+                    endIcon={<DeleteIcon />}
+                  >
+                    Удалить
+                  </Button>
+                </div>
+              ))}
+            </>
+          )
+            : (<h3>У вас нет новых событий</h3>)}
+        </Box>
       </Box>
-    </Box>
-  </Modal>
-);
+    </Modal>
+  );
+};
 
 export default EventModal;
